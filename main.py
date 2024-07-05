b3128f07a57560e50277b8051b0143a28bdc23cd
RYOX- DEV <167686870+Ryox97@users.noreply.github.com>
2024-07-05 14:09:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "Leader login",
+            "leader login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
